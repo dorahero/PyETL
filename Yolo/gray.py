bbbba4f66095fccd95efc6fc4caffc06f6e9cf0d
dorahero <dorahero2727@gmail.com>
2020-07-29 19:56:09 +0800

    commit

--- Yolo/gray.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import glob
-
-
-img = glob.glob('./data_original/*/*.jpg')
-for i in img:
-    image = cv2.imread(i)
-    image_g = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(i, image_g)
